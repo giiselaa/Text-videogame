aff89fdab1792388631c371d486bf7a39cbb1af7
giise <gisela.gonzalez@edu.uah.es>
2023-03-02 16:27:09 +0100

    idea

---
+++ videogame.py
@@ -0,0 +1,30 @@
+class Room:
+    """ En esta sala empieza el juego"""
+    def __init__(self, description, north, south, east, west):
+        """metodo constructor, inicializa"""
+        self.description = description
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+
+def main():
+    room_list = []
+
+    room = Room('Te encuentras en los pasadizos secretos de un castillo habitado por el principe bastardo.'
+                'Fuiste atrapado y llevado a las mazmorras pero conseguiste escapar, deberas encontrar la salida'
+                'antes de que noten la falta de tu presencia', 3, 4, 1, 2)
+
+    room = Room('Has llegado a una sala de reuniones secretas. Encuentras unos socumentos'
+                'sobre la mesa y descubres que el principe esta planeando un golpe para ser el rey. ¡No puedes'
+                'permitir que eso pase!', None, None, None, None)
+
+    room = Room('¡Oh no, un guardia! debes dar la vuelta', None, None, None, None)
+
+    room = Room('', None, None, None, None)
+
+    room_list.append(room)
+
+    current_room = 0
+
+    main()
