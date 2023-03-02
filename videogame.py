cac9674d667e762566485b03abd77d4b2208ffdb
giise <gisela.gonzalez@edu.uah.es>
2023-03-02 16:33:21 +0100

    idea1

--- videogame.py
+++ videogame.py
@@ -19,9 +19,12 @@ def main():
                 'sobre la mesa y descubres que el principe esta planeando un golpe para ser el rey. ¡No puedes'
                 'permitir que eso pase!', None, None, None, None)
 
-    room = Room('¡Oh no, un guardia! debes dar la vuelta', None, None, None, None)
+    room = Room('¡Oh no, un guardia! debes dar la vuelta o te volvera a atrapar', None, None, None, None)
 
-    room = Room('', None, None, None, None)
+    room = Room('Esta debe ser la sala de interrogatorios para los intrusos... No me da buena espina, salgamos de aqui'
+                , None, None, None, None)
+
+    room = Room('Hacia el este hay una puerta muy grande, ¡Podria ser la salida!', None, None, None, None)
 
     room_list.append(room)
 
